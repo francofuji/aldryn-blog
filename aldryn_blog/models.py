618f9e71db2d3de6309a84799a62849485b592d2
Yoandym <yoandym@gmail.com>
2015-10-22 11:05:32 -0300

    quitando opciones innecesarias

--- aldryn_blog/models.py
+++ aldryn_blog/models.py
@@ -179,8 +179,6 @@ class Post(models.Model):
     coauthors = models.ManyToManyField(
         to=AUTH_USER_MODEL,
         verbose_name=_('Co-Authors'),
-        null=True,
-        blank=True,
         related_name='aldryn_blog_coauthors'
     )
     publication_start = models.DateTimeField(
